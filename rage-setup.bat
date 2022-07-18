a448a3168df07667669b57ce1131fbd6bd69b592
monstyy <92098215+monstyy@users.noreply.github.com>
2022-07-18 16:33:26 +0200

    Implement script updates automatically in rage-setup

--- rage-setup.bat
+++ rage-setup.bat
@@ -69,9 +69,41 @@ if %winbuild% GEQ 10586 reg query "HKCU\Console" /v ForceV2 2>nul | find /i "0x0
 
 ::========================================================================================================================================
 color 07
-call ./fixes/findPID.bat
 title RAGEMP Optimization Script
+echo Checking for new script updates...
+for /f "tokens=1,* delims=:" %%A in ('curl -ks https://api.github.com/repos/monstyy/RAGEMP-Opt-Script/releases/latest ^| find "browser_download_url"') do (
+    set url=%%B
+)
+::========================================================================================================================
+for %%a in ("%url%") do (
+   set urlPath=!url:%%~NXa=!
+   set urlName=%%~NXa
+)
+set _urlName=%urlName:"=%
+echo Latest version: %_urlName%
+::========================================================================================================================
+if exist .\fixes\updates\%_urlName% (
+    echo Current version: %_urlName%
+    echo You already have the latest version.
+) else ( 
+    echo Downloading the latest version from the github repository
+    for /f "tokens=1,* delims=:" %%A in ('curl -ks https://api.github.com/repos/monstyy/RAGEMP-Opt-Script/releases/latest ^| find "browser_download_url"') do (
+        curl -kOL %%B
+        set url=%%B
+        for %%a in ("%url%") do (
+            set urlPath=!url:%%~NXa=!
+            set urlName=%%~NXa
+        )
+        set _urlName=%urlName:"=%
+        powershell Expand-Archive "%_urlName%" -destinationpath %cd% -force
+        move /y ".\%_urlName%" ".\fixes\updates"
+    )
+)
+::========================================================================================================================
+call ./fixes/findPID.bat
 taskkill /fi "PID ne %errorlevel%" /im cmd.exe /t /f
+del findPID.exe
+pause
 call ./fixes/main.bat
 ::========================================================================================================================================
 :RAGEOptEnd
